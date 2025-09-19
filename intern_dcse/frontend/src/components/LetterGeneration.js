73075fb03aca6b8fd03656d1f75190e6d5210f57
Oviyashree-CJ <oviyashreechris@gmail.com>
2025-09-19 11:00:30 +0530

    done

--- intern_dcse/frontend/src/components/LetterGeneration.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DocumentGeneratorPage from './DocumentGeneratorPage';
-
-const LetterGeneration = () => {
-  return <DocumentGeneratorPage />;
-};
-
-export default LetterGeneration;
